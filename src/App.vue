7235058aa984d461b5473aaedb96d7a9cf630635
haz145 <saralexandramarques@gmail.com>
2022-10-07 23:12:54 +0100

    bg fix; contacts section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import DeptsSection from "./components/DeptsSection.vue";
+import ContactsSection from "./components/ContactsSection.vue";
 </script>
 
 <template>
-  <div class="bg-white">
+  <div class="bg-gradient-to-r from-cr-purple via-cr-teal to-cr-orange px-32 pt-8">
+    
+    <img src="./imgs/logo.webp" alt="logo clube rainbow" class="w-36 h-36 flex justify-center mx-auto -mb-12">
+    
+    <div class="bg-cr-beige text-cr-brown px-8">
 
-    <div class="bg-cr-beige text-cr-brown mx-28 px-8">
-
-      <h1 class="text-5xl text-center font-shrikhand p-8">
+      <h1 class="text-5xl text-center font-shrikhand pt-16 mx-4 mb-4">
         Clube Rainbow
       </h1>
 
@@ -15,17 +18,14 @@ import DeptsSection from "./components/DeptsSection.vue";
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
 
-      <p class="mb-8">
-        insta + discord + email
-      </p>
-
+      <ContactsSection class="mb-8" />
       <DeptsSection />
 
-      <p>
+      <p class ="font-shrikhand text-2xl mb-2">
         Candidaturas
       </p>
 
-      <p>
+      <p class ="font-shrikhand text-2xl pb-2" >
         Parcerias
       </p>
 
---
+++ src/components/ContactsSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex justify-center mx-auto gap-8">
+        
+        <span>
+            <img src="../imgs/insta_icon.webp" alt="instagram" class="h-8 w-8 inline mr-1"> 
+            <a href="https://www.instagram.com/clube.rainbow/" class="hover:underline">@clube.rainbow</a>
+        </span>
+
+        <span>
+            <img src="../imgs/discord_icon.webp" alt="discord" class="h-8 w-8 inline mr-1"> 
+            <a href="" class="hover:underline">Discord</a>
+        </span>
+
+        <span>
+            <img src="../imgs/email_icon.webp" alt="email" class="h-8 w-8 inline"> 
+            rainbow.clube.estudantes@gmail.com
+        </span>
+    
+    </div>
+</template>
