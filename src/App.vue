d680c5355871971d615345b89b720f5884dfc90f
haz145 <saralexandramarques@gmail.com>
2022-10-07 20:03:07 +0100

    depts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <script setup lang="ts">
+import DeptInfo from "./components/DeptInfo.vue";
+import { Dept } from "./components/DeptInfo.vue";
+
+const depts : Dept[] = [
+  {
+    name: "Recreativo",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit",
+    members: ["a","b","c"],
+  },
+  {
+    name: "de Comunicação",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit",
+    members: ["d","e","f"],
+  },
+  {
+    name: "Pedagógico",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit",
+    members: ["g","h","i"],
+  }
+]
 </script>
 
 <template>
-  <div class="bg-cr-beige p-8">
-    <h1 class="text-3xl font-shrikhand text-cr-brown">
-      Hello world!
+  <div class="bg-cr-beige text-cr-brown px-20">
+    
+    <h1 class="text-5xl text-center font-shrikhand p-8">
+      Clube Rainbow
     </h1>
-  </div>
 
+    <p class="mb-8">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+      <div v-for="(dept, index) in depts" :key="index">
+          <DeptInfo :dept="dept" />
+      </div>
+    </div>
+
+    <p class="">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+    </p>
+
+  </div>
 </template>
---
+++ src/components/DeptInfo.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+export interface Dept {
+    name: string;
+    desc: string;
+    members: string[];
+}
+</script>
+
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+defineProps<{ dept: Dept }>()
+
+const showmembers : Ref<boolean> = ref(false);
+</script>
+
+<template>
+  <div class="flow-root border border-cr-brown rounded-xl pt-4 px-6 pb-2">
+      
+    <h1
+      class ="font-shrikhand text-2xl mb-2"        
+      :class="{
+          'text-cr-purple': dept.name === 'Recreativo',
+          'text-cr-teal': dept.name === 'de Comunicação',
+          'text-cr-orange': dept.name === 'Pedagógico',
+        }">
+        Departamento {{ dept.name }}
+    </h1>
+
+    <p class = "text-cr-brown">
+      {{ dept.desc }}
+    </p>
+
+    <button class="text-sm font-bold float-right hover:underline"
+    @click="showmembers = !showmembers">
+      <p v-if="showmembers">- Membros</p>
+      <p v-else>+ Membros</p>
+    </button>
+
+    <ul v-show="showmembers" class="pt-4">
+      <li v-for="(member, index) in dept.members" :key="index">
+        • {{ member }}
+      </li>
+    </ul>
+
+  </div>
+</template>
