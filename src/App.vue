3c1941987fc9de659ed175231f9db6cfb4c82810
haz145 <saralexandramarques@gmail.com>
2022-10-08 01:02:01 +0100

    some responsiveness

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@ import ContactsSection from "./components/ContactsSection.vue";
 </script>
 
 <template>
-  <div class="bg-gradient-to-r from-cr-purple via-cr-teal to-cr-orange px-32 pt-8">
+  <div class="bg-gradient-to-r from-cr-purple via-cr-teal to-cr-orange px-[9vw] pt-8">
     
-    <img src="./imgs/logo.webp" alt="logo clube rainbow" class="w-36 h-36 flex justify-center mx-auto -mb-12">
+    <img src="./imgs/logo.webp" alt="logo clube rainbow" class="w-32 h-32 md:w-36 md:h-36 flex justify-center mx-auto -mb-12">
     
     <div class="bg-cr-beige text-cr-brown px-8">
 
-      <h1 class="text-5xl text-center font-shrikhand pt-16 mx-4 mb-4">
+      <h1 class="text-4xl md:text-5xl text-center font-shrikhand pt-16 mx-4 mb-4">
         Clube Rainbow
       </h1>
 
-      <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mb-4">
+      <div class="grid grid-cols-2 text-sm md:text-base md:grid-cols-3 gap-4 mb-4">
         <p class="col-span-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
           dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
--- src/components/DeptInfo.vue
+++ src/components/DeptInfo.vue
@@ -7,7 +7,7 @@ const showmembers : Ref<boolean> = ref(false);
 </script>
 
 <template>
-  <div class="flow-root text-sm border border-cr-brown rounded-xl pt-4 px-6 pb-2">
+  <div class="flow-root border border-cr-brown rounded-xl pt-4 px-6 pb-2">
       
     <h1
       class ="font-shrikhand text-2xl mb-2"        
@@ -19,21 +19,23 @@ const showmembers : Ref<boolean> = ref(false);
         Departamento {{ dept.name }}
     </h1>
 
-    <p class = "text-cr-brown mb-2">
-      {{ dept.desc }}
-    </p>
+    <div class="text-xs md:text-sm">
+      <p class = "text-cr-brown mb-2">
+        {{ dept.desc }}
+      </p>
 
-    <button class="font-bold float-right hover:underline"
-    @click="showmembers = !showmembers">
-      <p v-if="showmembers">- Membros</p>
-      <p v-else>+ Membros</p>
-    </button>
+      <button class="font-bold float-right hover:underline"
+        @click="showmembers = !showmembers">
+        <p v-if="showmembers">- Membros</p>
+        <p v-else>+ Membros</p>
+      </button>
 
-    <ul v-show="showmembers" class="pt-4">
-      <li v-for="(member, index) in dept.members" :key="index">
-        • {{ member }}
-      </li>
-    </ul>
-
+      <ul v-show="showmembers" class="pt-4">
+        <li v-for="(member, index) in dept.members" :key="index">
+          • {{ member }}
+        </li>
+      </ul>
+    </div>
+  
   </div>
 </template>
