823aaf9c1a70f67ae710c8d0a92f4192cd8619b7
haz145 <saralexandramarques@gmail.com>
2023-02-14 21:35:15 +0000

    dynamic imgs fix

--- src/components/About/PartnersSection.vue
+++ src/components/About/PartnersSection.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
     import partners_json from '../../jsons/partners.json';
+    const getImg = (img: string) => {
+        return new URL('../../imgs/partners/' + img, import.meta.url).href
+    }
 </script>
 
 <template>
@@ -9,7 +12,7 @@
         </p>
         <div class="flex flex-wrap justify-center md:justify-start items-center gap-4 mb-2">
             <a v-for="(partner, i) in partners_json.partners" :key="i" class="rounded-xl hover:outline" :href="partner.link" target="_blank">
-                <img :src="'/src/imgs/partners/'+partner.logo" :alt="partner.name" 
+                <img :src="getImg(partner.logo)" :alt="partner.name" 
                 class="w-20 h-20 rounded-xl" />
             </a>
         </div>
