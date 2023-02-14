b217444e16a5fbc4b1543d2c1cee6bb88a96b851
haz145 <saralexandramarques@gmail.com>
2023-02-14 23:58:27 +0000

    404 html

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 export const router = createRouter({
-  history: createWebHistory('cluberainbow.github.io'),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
