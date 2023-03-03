30c43604d8539a726324e6e23958803397f8efa0
haz145 <saralexandramarques@gmail.com>
2023-03-03 00:36:40 +0000

    merch page wip

---
+++ src/components/Merch/Slides.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const MAX = 10;
+    const img_id = ref(1);
+
+    const browse = (direction: 'right' | 'left') => {
+        if (direction === 'right') {
+            if (img_id.value < MAX)
+                img_id.value += 1
+            else 
+                img_id.value = 1
+        }
+        else if (direction === 'left') {
+            if (img_id.value > 1)
+                img_id.value -= 1
+            else
+                img_id.value = MAX
+        }
+    }
+
+    interface Props {
+        path: string;
+        multiple: boolean;
+    }
+    defineProps<Props>();
+</script>
+
+<template>
+    <div class="flex items-center gap-2">
+        <button v-if="multiple" @click="browse('left')">
+            <img src="../../assets/arrow_down.svg" alt="toggle list" class="h-5 w-5 rotate-90 hover:brightness-110"/>
+        </button>
+
+        <div class="flex items-center justify-center border-2 rounded-xl bg-white px-4 w-48 h-36">
+            <img :src="path + img_id + '.png'" alt="bandeira" class="drop-shadow-2xl animation" />
+        </div>
+
+        <button v-if="multiple" @click="browse('right')">
+            <img src="../../assets/arrow_down.svg" alt="toggle list" class="h-5 w-5 -rotate-90 hover:brightness-110"/>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+    /* ... */
+</style>
--- src/pages/Merch.vue
+++ src/pages/Merch.vue
@@ -1,8 +1,38 @@
 <script setup lang="ts">
+    import Divider from '../components/Divider.vue';
+    import Slides from '../components/Merch/Slides.vue';
 </script>
 
 <template>
-    <div>
-        merch
+    <div class="flex flex-col gap-4">
+        
+        <p class="text-4xl font-shrikhand font-normal text-outline-white whitespace-nowrap">
+            Apoia-nos
+        </p>
+
+        <div>
+            <p>
+                Somos uma iniciativa sem fins lucrativos, no entanto precisamos sempre de fundos para 
+                nos ajudar a organizar e publicitar eventos futuros. 
+            </p>
+            <p>
+                Não pedimos doações, mas temos autocolantes disponíveis para venda nos nossos eventos.
+            </p>
+        </div>
+
+        <Divider />
+
+        <div class="flex flex-wrap gap-5 justify-center lg:justify-start">
+            <Slides path="/stickers/flags/" :multiple="true" />
+            <div class="flex flex-col">
+                <p class="text-2xl font-shrikhand font-normal">
+                    Bandeiras
+                </p>
+                <p> Bandeiras de várias identidades LGBT+. </p>
+                <p> Disponível nos nossos eventos! </p>
+                <p class="mt-auto"> work in progress </p>
+            </div>
+        </div>
+        
     </div>
 </template>
