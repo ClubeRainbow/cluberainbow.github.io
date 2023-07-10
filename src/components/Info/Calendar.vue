f1355806904eabdd3a0d4a81de71c083a9832faa
haz145 <saralexandramarques@gmail.com>
2023-07-10 16:32:03 +0100

    calendar post

--- src/components/Info/Calendar.vue
+++ src/components/Info/Calendar.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
     interface Props {
-        next_event: string
+        next_event: {
+            link: string,
+            img: string
+        }
     }
     defineProps<Props>();
 </script>
@@ -12,8 +15,26 @@
             Calendário
         </p>
 
-        <p>
-            under construction
-        </p>
+        <div class="flex flex-wrap gap-y-4 gap-x-12">  
+            
+            <div class="flex flex-col gap-2 w-full md:w-auto">
+                <p class="text-2xl font-shrikhand font-normal text-outline-white">
+                    Próximo Evento:
+                </p>
+                <a class="mx-auto md:mx-0" :href="next_event.link" target="_blank">
+                    <img :src="next_event.img" alt="próximo evento" class="max-w-96 max-h-96 border-2 rounded-xl" />
+                </a>
+            </div>
+            
+            <div class="flex flex-col gap-2">
+                <p class="text-2xl font-shrikhand font-normal text-outline-white">
+                    Publicações & Atividades:
+                </p>
+                <p>
+                    under construction
+                </p>
+            </div>
+        </div>
+
     </div>
 </template>
