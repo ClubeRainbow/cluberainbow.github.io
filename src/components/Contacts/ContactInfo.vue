8c73d5647d96d5fc108e7c1716e1fc540723a979
haz145 <saralexandramarques@gmail.com>
2023-07-17 23:08:01 +0100

    simplified contacts panda

--- src/components/Contacts/ContactInfo.vue
+++ src/components/Contacts/ContactInfo.vue
@@ -3,21 +3,10 @@
 </script>
 
 <template>
-    <div class="flex flex-wrap lg:flex-col gap-x-4 justify-center items-center">
-        <img src="../../assets/kaku2.png" alt="panda" class="h-52 w-52 md:h-60 md:w-60 lg:h-72 lg:w-72"/>
-        <div class="contact_box">
+    <div class="flex flex-col gap-x-6 justify-center items-center">
+        <img src="../../assets/kaku2.png" alt="panda" class="h-52 md:h-60 lg:h-64"/>
+        <div class="border-y-2 py-4 px-8 w-full flex justify-center">
             <ContactIcons class="flex flex-col gap-3" size="h-8 w-8 md:h-10 md:w-10" :text="true" />
         </div>
     </div>
 </template>
-
-<style scoped>
-    .contact_box {
-        @apply flex justify-center
-        [@media(max-width:546px)]:border-y-2 
-        [@media(max-width:546px)]:py-4 
-        [@media(max-width:546px)]:px-8 
-        [@media(max-width:546px)]:w-full 
-        lg:border-y-2 lg:py-4 lg:px-8
-    }
-</style>
