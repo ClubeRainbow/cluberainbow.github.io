17b6dd19898036fcee9b93e25ca123f282fa68fd
haz145 <saralexandramarques@gmail.com>
2023-07-17 23:13:46 +0100

    padding fix

--- src/components/Contacts/ContactInfo.vue
+++ src/components/Contacts/ContactInfo.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <div class="flex flex-col gap-x-6 justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
         <img src="../../assets/kaku2.png" alt="panda" class="h-52 md:h-60 lg:h-64"/>
-        <div class="border-y-2 py-4 px-8 w-full flex justify-center">
+        <div class="border-y-2 py-6 px-8 w-full flex justify-center items-center">
             <ContactIcons class="flex flex-col gap-3" size="h-8 w-8 md:h-10 md:w-10" :text="true" />
         </div>
     </div>
