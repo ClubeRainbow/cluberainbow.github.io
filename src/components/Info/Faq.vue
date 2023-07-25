7b61f58e8d55eb9b52bfc2a02c23718dc2a84859
haz145 <saralexandramarques@gmail.com>
2023-07-25 20:33:41 +0100

    faq

--- src/components/Info/Faq.vue
+++ src/components/Info/Faq.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import info from '../../jsons/info.json';
+    import FaqItem from "./FaqItem.vue";
+    import info from '../../jsons/info.json';
 </script>
 
 <template>
@@ -9,48 +10,163 @@ import info from '../../jsons/info.json';
             FAQ
         </p>
 
-        <div class="flex flex-col gap-2">
-            <p class="subsubtitle">
-                ✦ Como posso fazer parte? ✦
-            </p>
+        <FaqItem question="Como posso fazer parte?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    Para fazeres parte da organização do Clube Rainbow, <b>deves candidatar-te a 
+                    um dos nossos departamentos -
+                    <button class="click" @click="$router.push({name: 'about', hash: '#depts'})">
+                        podes saber mais sobre eles aqui!
+                    </button></b>
+                </p>
+                <p>
+                    As candidaturas abrem sempre pelo menos uma vez por ano, por isso se não te puderes
+                    candidatar agora, terás sempre outras oportunidades.
+                    Por favor tem em atenção que <b>apenas aceitamos estudantes e alumni da Universidade do Minho.</b>
+                </p>
+                <p>
+                    Atualmente, <b>as candidaturas estão 
+                    {{ (info.candidaturas.ped || info.candidaturas.recr || info.candidaturas.comms) ? 'abertas.' : 'fechadas.'}}</b>
+                </p>
+            </div>
+            
+        </FaqItem>
 
-            <p>
-                Todes são bem vindes a participar nos eventos do Clube Rainbow,
-                mas caso queiras fazer parte da sua organização, deves candidatar-te a 
-                um dos nossos departamentos -
-                <button class="click" @click="$router.push({name: 'about', hash: '#depts'})">
-                    sabe mais sobre eles aqui!
-                </button>
-            </p>
-        </div>
+        <FaqItem question="O Clube Rainbow é apenas para estudantes da UM?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    O Clube Rainbow foi criado com es estudantes da Universidade do Minho em mente, 
+                    mas <b>todes são bem-vindes a participar nos nossos eventos e atividades!</b>
+                </p>
+                <p>
+                    Apenas a organização do Clube em si é gerida apenas por estudantes e alumni da UM.
+                </p>
+            </div>
+        </FaqItem>
 
-        <div class="flex flex-col gap-2">
-            <p class="subsubtitle">
-                ✦ Quem pode participar nos eventos? ✦
-            </p>
-            <p>
-                aaa
-            </p>
-        </div>
+        <FaqItem question="Pessoas fora da comunidade LGBTQIA+ podem participar?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    Claro! Embora o Clube seja especialmente direcionado para membros da comunidade LGBTQIA+, 
+                    <b>nunca iremos excluir ninguém das nossas atividades por não fazer parte da comunidade.</b>
+                </p>
+                <p>
+                    Afinal, não queremos apenas criar um grupo que nos aproxime de pessoas com uma realidade mais próxima da nossa 
+                    - queremos também mostrar a nossa realidade aos outros.
+                </p>
+            </div>
+        </FaqItem>
 
-        <div class="flex flex-col gap-2">
-            <p class="subsubtitle">
-                ✦ Onde/quando são os eventos? ✦
-            </p>
-            <p>
-                aaa
-            </p>
-        </div>
+        <FaqItem question="Onde/quando são os eventos?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    Os nossos eventos decorrem <b>sobretudo no campus de Gualtar da Universidade do Minho</b>, mas ocasionalmente
+                    poderemos organizar algo no resto da cidade de Braga. Futuramente, gostaríamos também de organizar eventos
+                    no campus de Azurém!
+                </p>
+                <p>
+                    Relativamente à frequência dos eventos, 
+                    <button class="click" @click="$router.push({name: 'info', hash: '#events'})">
+                        consulta a nossa calendarização acima.
+                    </button>
+                </p>
+            </div>
+        </FaqItem>
 
-        <div class="flex flex-col gap-2">
-            <p class="subsubtitle">
-                ✦ Posso entrar no Discord se... ✦
-            </p>
-            <p>
-                aaa
-            </p>
-        </div>
+        <FaqItem question="É preciso inscrição para os eventos?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    <b>Não é necessária inscrição para a maior parte dos nossos eventos, mas quando tal for necessária, iremos indicá-lo 
+                    junto com o anúncio do evento no nosso Instagram.</b>
+                    Normalmente, esta inscrição serve só para fins estatísticos - por isso podes aparecer 
+                    mesmo que não te tenhas inscrito.
+                </p>
+                <p>
+                    Os nossos eventos são também sempre de entrada gratuita.
+                </p>
+            </div>
+        </FaqItem>
 
+        <FaqItem question="Posso comprar merch fora dos eventos?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    <b>Atualmente, não.</b> Apenas vendemos merch nos nossos eventos ou quando vamos a marchas do pride, pois,
+                    de momento, não teríamos facilidade em realizar vendas de outro modo para todes os interessades.
+                </p>
+            </div>
+        </FaqItem>
+
+        <FaqItem question="O que é o Discord?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    O Discord é uma aplicação de chat, que permite criar e participar em vários servidores.
+                    Cada servidor opera como um chat de grupo com vários canais ou fóruns, 
+                    que podem ser dedicados a diferentes temas.
+                </p>
+                <p>
+                    O Clube Rainbow tem um servidor de Discord próprio, sendo este <b>uma plataforma de socialização
+                    mais informal, onde poderás conhecer outras pessoas LGBTQIA+, falar dos teus hobbies, 
+                    participar em eventos online, entre outros!</b>
+                    Graças ao Discord, garantimos que existe sempre algum espaço disponível para nós mesmo quando
+                    não podemos estar juntes num espaço físico.
+                </p>
+                <p>
+                    O servidor dispõe também de canais privados que são utilizados exclusivamente pela organização
+                    do Clube para o planeamento de eventos e afins. Daí ser necessário utilizares a aplicação
+                    caso queiras fazer parte dos departamentos do Clube.
+                </p>
+                <p><b>
+                    Para te juntares ao nosso servidor do Discord, consulta a nossa 
+                    <button class="click" @click="$router.push({name: 'contacts'})">
+                        página de contactos
+                    </button>
+                    ou 
+                    <a class="click" href="https://discord.gg/5Z9YshrZah" target="_blank">
+                        utiliza este link!
+                    </a>
+                </b></p>
+            </div>
+        </FaqItem>
+
+        <FaqItem question="Posso entrar no servidor do Discord se não for estudante da UM?">
+            <div class="flex flex-col gap-2">
+                <p>
+                    Sim! O servidor do Discord, tal como os nossos eventos, é <b>aberto a todes</b>.
+                    Podes entrar mesmo que não estudes na UM ou não sejas LGBTQIA+.
+                </p>
+            </div>
+        </FaqItem>
+
+        <FaqItem question="Preciso de ajuda!">
+            <div class="flex flex-col gap-2">
+                <p>
+                    O Clube Rainbow não é qualificado para prestar apoio de emergência,
+                    mas existem várias outras associações que te poderão ajudar!
+                </p>
+                <p>
+                    <b>
+                        Consulta a nossa 
+                        <button class="click" @click="$router.push({name: 'resources'})">
+                            página de recursos úteis,
+                        </button>
+                    </b>
+                    onde poderás encontrar os contactos de várias associações portuguesas
+                    que te poderão prestar auxílio, dependendo daquilo que precisas.
+                </p>
+            </div>
+        </FaqItem>
+
+        <FaqItem question="Tenho outra dúvida...">
+            <div class="flex flex-col gap-2">
+                <p>
+                    Sem problema,
+                    <button class="click" @click="$router.push({name: 'contacts'})">
+                        entra em contacto connosco
+                    </button>
+                    e teremos todo o gosto em ajudar!
+                </p>
+            </div>
+        </FaqItem>
 
     </div>
 </template>
---
+++ src/components/Info/FaqItem.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    const show_answer = ref(false)
+
+    interface Props {
+        question: string;
+    }
+    defineProps<Props>();
+</script>
+
+<template>
+    <div class="flex flex-col gap-2 box px-4 md:px-6 py-3 md:py-4">
+        
+        <button class="subsubtitle question_btn" @click="show_answer = !show_answer">
+            {{ question }}
+            <img src="../../assets/arrow_down.svg" alt="toggle list" class="h-4 w-4" :class="{ 'rotate-180' : show_answer }"/>
+            
+        </button>
+
+        <Transition> 
+            <slot v-if="show_answer"></slot>
+        </Transition>
+    </div>
+</template>
+
+<style scoped>
+    .question_btn {
+        @apply flex items-center justify-between md:justify-start text-left gap-3
+        hover:brightness-125 focus:brightness-125 hover:underline focus:underline
+    }
+
+    .v-enter-active, .v-leave-active {
+        max-height: 250px;
+        transition: max-height 0.5s ease;
+        overflow: hidden;
+    }
+    .v-enter-from, .v-leave-to {    
+        max-height: 0px;
+        transition: max-height 0.5s ease;
+        overflow: hidden;
+    }
+</style>
