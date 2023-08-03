3bf853e81ca9e0f0e55b680aa84ddc0cde71717e
haz145 <haz145@users.noreply.github.com>
2023-08-03 17:17:17 +0000

    deploy: 9260c4eb6d22d55d8e6c74574572ea36a0ce341c

---
+++ assets/candidaturas.4728367f.js
@@ -0,0 +1,1 @@
+const o="xx/xx/xxxx",e={open:!1,link:"https://google.com"},t={open:!0,link:"https://google.com"},n={open:!0,link:"https://google.com"},c={open:!0,timelimit:o,ped:e,recr:t,comms:n};export{c};
--- assets/info.ca207433.js
+++
@@ -1,1 +0,0 @@
-const o={open:!0,timelimit:"xx/xx/xxxx",ped:{open:!1,link:"https://google.com"},recr:{open:!0,link:"https://google.com"},comms:{open:!0,link:"https://google.com"}},e={cands:o};export{e as i};
