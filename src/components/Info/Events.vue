dc53d8d570223e51d801cbee7ef2ca7a557b90e5
haz145 <saralexandramarques@gmail.com>
2023-08-05 22:29:34 +0100

    improved events section

--- src/components/Info/Events.vue
+++ src/components/Info/Events.vue
@@ -8,81 +8,79 @@
 
         <h1> Eventos </h1>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-4 gap-x-8">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-6">
+            
+            <NextEvent class="lg:hidden"/>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-y-4 gap-x-8 md:col-span-2">
-                <div class="flex flex-col gap-1">
-                    <h3>Palestras</h3>
-                    <p>
-                        Palestras com diferentes oradores convidades para falar sobre diversos temas
-                        relacionados com a comunidade LGBTQIA+.
-                    </p>
-                    <p>
-                        <span class="font-shrikhand">Próximo: </span>
-                        <a v-if="events.next.link && events.next.type === 'palestra'" :href="events.next.link" class="click"
-                            target="_blank">
-                            {{ events.next.name }}, {{ events.next.date }}
-                        </a>
-                        <span v-else>Por anunciar...</span>
-                    </p>
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <h3>Meets</h3>
-                    <p>
-                        Encontros organizados pelo Clube Rainbow para falarmos
-                        entre nós sobre diversas temáticas LGBTQIA+.
-                    </p>
-                    <p>
-                        <span class="font-shrikhand">Próximo: </span>
-                        <a v-if="events.next.link && events.next.type === 'meet'" :href="events.next.link" class="click"
-                            target="_blank">
-                            {{ events.next.name }}, {{ events.next.date }}
-                        </a>
-                        <span v-else>Por anunciar...</span>
-                    </p>
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <h3>Hangouts</h3>
-                    <p>
-                        Oportunidades de convívio mais casuais.
-                    </p>
-                    <p>
-                        <span class="font-shrikhand">Próximo: </span>
-                        <a v-if="events.next.link && events.next.type === 'hangout'" :href="events.next.link" class="click"
-                            target="_blank">
-                            {{ events.next.name }}, {{ events.next.date }}
-                        </a>
-                        <span v-else>Por anunciar...</span>
-                    </p>
-                </div>
-
-                <div class="flex flex-col gap-1">
-                    <h3>Eventos Online</h3>
-                    <p>
-                        Eventos online casuais no nosso
-                        <a class="click" href="https://discord.gg/5Z9YshrZah" target="_blank">
-                            servidor de Discord.
-                        </a>
-                    </p>
-
-                    <p>
-                        <span class="font-shrikhand">Próxima Game Night: </span>
-                        <span>{{ events.online.game_night ? events.online.game_night : 'Por anunciar...' }}</span>
-                    </p>
-
-                    <p>
-                        <span class="font-shrikhand">Próxima Film Night: </span>
-                        <span>{{ events.online.film_night ? events.online.film_night : 'Por anunciar...' }}</span>
-                    </p>
-                </div>
-
+            <div class="flex flex-col gap-2 box py-3 md:py-4 px-5 md:px-6">
+                <h3>Palestras</h3>
+                <p>
+                    Palestras com diferentes oradores convidades para falar sobre diversos temas
+                    relacionados com a comunidade LGBTQIA+.
+                </p>
+                <p>
+                    <span class="font-shrikhand">Próximo: </span>
+                    <a v-if="events.next.link && events.next.type === 'palestra'" :href="events.next.link" class="click"
+                        target="_blank">
+                        {{ events.next.name }}, {{ events.next.date }}
+                    </a>
+                    <span v-else>Por anunciar...</span>
+                </p>
             </div>
 
-            <NextEvent class="md:col-span-2 xl:col-span-1"/>
+            <NextEvent class="hidden lg:flex row-span-2 xl:row-span-3 2xl:row-span-4"/>
+
+            <div class="flex flex-col gap-2 box py-3 md:py-4 px-5 md:px-6">
+                <h3>Meets</h3>
+                <p>
+                    Encontros organizados pelo Clube Rainbow para falarmos
+                    entre nós sobre diversas temáticas LGBTQIA+.
+                </p>
+                <p>
+                    <span class="font-shrikhand">Próximo: </span>
+                    <a v-if="events.next.link && events.next.type === 'meet'" :href="events.next.link" class="click"
+                        target="_blank">
+                        {{ events.next.name }}, {{ events.next.date }}
+                    </a>
+                    <span v-else>Por anunciar...</span>
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-2 box py-3 md:py-4 px-5 md:px-6">
+                <h3>Hangouts</h3>
+                <p>
+                    Oportunidades de convívio mais casuais.
+                </p>
+                <p>
+                    <span class="font-shrikhand">Próximo: </span>
+                    <a v-if="events.next.link && events.next.type === 'hangout'" :href="events.next.link" class="click"
+                        target="_blank">
+                        {{ events.next.name }}, {{ events.next.date }}
+                    </a>
+                    <span v-else>Por anunciar...</span>
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-2 box py-3 md:py-4 px-5 md:px-6 xl:col-span-2 2xl:col-span-1">
+                <h3>Eventos Online</h3>
+                <p>
+                    Eventos online casuais no nosso
+                    <a class="click" href="https://discord.gg/5Z9YshrZah" target="_blank">
+                        servidor de Discord.
+                    </a>
+                </p>
+
+                <p>
+                    <span class="font-shrikhand">Próxima Game Night: </span>
+                    <span>{{ events.online.game_night ? events.online.game_night : 'Por anunciar...' }}</span>
+                </p>
+
+                <p>
+                    <span class="font-shrikhand">Próxima Film Night: </span>
+                    <span>{{ events.online.film_night ? events.online.film_night : 'Por anunciar...' }}</span>
+                </p>
+            </div>
 
         </div>
-
     </div>
 </template>
--- src/components/Info/NextEvent.vue
+++ src/components/Info/NextEvent.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-    <div class="flex flex-col gap-2 items-center mx-auto w-full">
+    <div class="flex flex-col gap-2 items-center justify-center text-center w-full box px-6 md:px-8 pb-6 pt-4 bg-cr-red-beige">
         <h3>Próximo Evento</h3>
         
         <div class="img_space">
-            <a v-if="events.next.img && events.next.link" :href="events.next.link" target="_blank" class="alt_highlight">
+            <a v-if="events.next.img && events.next.link" :href="events.next.link" target="_blank">
                 <img :src="events.next.img" alt="próximo evento" class="rounded-xl" />
             </a>
             <p v-else class="text-xl md:text-2xl font-shrikhand">
@@ -22,12 +22,12 @@
         @apply w-full aspect-square
         max-w-[300px] max-h-[300px] 
         md:max-w-[400px] md:max-h-[400px] 
-        xl:max-w-full xl:max-h-full 
+        xl:max-w-full xl:max-h-full
         flex items-center justify-center 
-        border-2 rounded-xl 
-        bg-cr-beige drop-shadow-md
+        border-2 rounded-xl bg-cr-beige
     }
-    .alt_highlight {
-        @apply hover:brightness-100 focus:brightness-100
+    .img_space > a:hover,
+    .img_space > a:focus {
+        @apply brightness-100
     }
 </style>
